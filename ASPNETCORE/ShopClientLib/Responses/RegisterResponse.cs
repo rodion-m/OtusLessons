36f0473100159986ef0b9013c609a3365e1f7576
Rodion Mostovoi <rodion.mostovoy@outlook.com>
2023-12-23 00:34:31 +0800

    Add ShopClientLib project and files, rename FluentValidationLesson
    A new project, ShopClientLib, has been added to the solution. This project includes new request and response models for user registration and login. Additionally, the naming of the FluentValidationLesson project has been revised to ValidationLesson for better alignment with project content.

---
+++ ASPNETCORE/ShopClientLib/Responses/RegisterResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson14.Models.Responses;
+
+public class RegisterResponse
+{
+    public RegisterResponse(string? message = null)
+    {
+        Message = message;
+    }
+
+    public string? Message { get; set; }
+    public string Token { get; set; }
+}
