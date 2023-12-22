32608f816b2820b8a330ead40c3fd59bfd95014c
Rodion Mostovoi <rodion.mostovoy@outlook.com>
2023-12-23 00:23:12 +0800

    Add 'FluentValidationLesson' and 'PollyLesson' projects and their test projects
    This commit adds two new projects: 'FluentValidationLesson' and 'PollyLesson', along with their test projects. Configuration and project files are created for each, including appsettings, launchSettings and .csproj files. Also a new 'Program.cs' file implemented for these projects. They have been added to the solution file.

--- ASPNETCORE/DomainEventsCQRS/DomainEventsWithMediatR/Program.cs
+++ ASPNETCORE/DomainEventsCQRS/DomainEventsWithMediatR/Program.cs
@@ -1,6 +1,7 @@
 using DomainEventsWithMediatR;
 using DomainEventsWithMediatR.DomainEvents.Events;
 using MediatR;
+using MediatR.NotificationPublishers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ScopedDependency>();
@@ -8,6 +9,9 @@ builder.Services.AddMediatR(cfg =>
 {
     //Обычно все обработчики событий находятся в слое Application
     cfg.RegisterServicesFromAssemblyContaining<Program>();
+
+    //cfg.NotificationPublisher = new ForeachAwaitPublisher(); // По умолчанию
+    cfg.NotificationPublisher = new TaskWhenAllPublisher();
 });
 
 var app = builder.Build();
